d557c5fd5177d651c2e9b8de7caa6a9dec4b0929
Chika Ejiofor <chikaejiofor95@yahoo.com>
2021-03-20 12:45:15 +0100

    ok

--- src/app/user/createuser/createuser.component.ts
+++ src/app/user/createuser/createuser.component.ts
@@ -79,4 +79,16 @@ export class CreateuserComponent implements OnInit {
     });
     this.submitted = false;
   }
+
+  omit_special_char(event) {
+    var k;
+    k = event.charCode; //         k = event.keyCode;  (Both can be used)
+    return (
+      (k > 64 && k < 91) ||
+      (k > 96 && k < 123) ||
+      k == 8 ||
+      k == 32 ||
+      (k >= 48 && k <= 57)
+    );
+  }
 }
